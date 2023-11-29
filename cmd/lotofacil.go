261a58cd6739feb65a4e8f2e893a578a839945ef
Alexandre Guerra <agmguerra@gmail.com>
2023-11-28 22:06:40 -0300

    gitignore adjust

--- cmd/lotofacil.go
+++ cmd/lotofacil.go
@@ -9,6 +9,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var NumCards int
+
 // lotofacilCmd represents the lotofacil command
 var lotofacilCmd = &cobra.Command{
 	Use:   "lotofacil",
@@ -22,14 +24,4 @@ a money prize with 11, 12, 13, 14, 15 matching numbers.`,
 
 func init() {
 	rootCmd.AddCommand(lotofacilCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lotofacilCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lotofacilCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
